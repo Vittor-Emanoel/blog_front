fa8aae843fc659fbbbfd269865752acf6061c2d4
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-05-10 15:50:05 -0300

    FEAT: add props in post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,28 +1,33 @@
-export default function Post() {
+import { format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+export default function Post({
+  title,
+  summary,
+  author,
+  cover,
+  content,
+  createdAt,
+}) {
   return (
     <div className="post">
       <div className="image">
-        <img
-          src="https://s2.glbimg.com/Cvu8OlsoKf93_yGcgetFi53QEYI=/418x342:3080x1839/fit-in/515x290/middle/smart/filters:strip_icc():strip_exif():format(webp)/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/B/w/vfsbNxQu64HCO0ZMY7pQ/ap22359353444009.jpg"
-          alt=""
-        />
+        <img src={"http://localhost:3333/" + cover} alt="" />
       </div>
 
       <div className="texts">
-        <h2>
-          Fortuna do rei Charles III chega a R$ 3,6 bilhões, diz jornal
-          britânico
-        </h2>
+        <h2>{title}</h2>
         <p className="info">
           <a href="" className="author">
-            Vittor Emanoel
+            {author?.username}
           </a>
-          <time>2023-04-17 00:00:00</time>
+          <time>
+            {format(new Date(createdAt), "d MMM, yyyy HH:mm", {
+              locale: ptBR,
+            })}
+          </time>
         </p>
-        <p className="summary">
-          De acordo com assessores entrevistados pelo jornal, Charles poupou
-          assiduamente os lucros que recebeu do Ducado da Cornualha.
-        </p>
+        <p className="summary">{summary}</p>
       </div>
     </div>
   );
